00d092f313dd927b01b3b7debe787b7740e39d31
JoniJnm <joni@jonijnm.es>
2014-05-04 12:31:10 +0200

    Mejoras modLcaCache

--- cache.php
+++ cache.php
@@ -1,21 +1,23 @@
 <?php 
 
 /**
-* @Copyright Copyright (C) 2012 - JoniJnm.es
+* @Copyright Copyright (C) 2014 - JoniJnm.es
 * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
 **/
 
 class modLcaCache {	
-	const CVERSION = "f4";
-	var $params;
-	var $on;
-	var $expired;
-	var $time;
-	var $path;
-	var $file;
-	var $xml;
-	var $show;
-        var $o_month;
+	const CVERSION = "f6";
+	
+	public $show;
+	public $o_month;
+	
+	private $params;
+	private $on;
+	private $expired;
+	private $time;
+	private $path;
+	private $file;
+	private $xml;
 	
 	function __construct(&$params) {
 		$this->params = $params;
@@ -25,39 +27,12 @@ class modLcaCache {
 		$this->path = JPATH_BASE.'/cache/mod_lca/';
 	}
 	function init() {
-		ob_start();
-		print_r($this->params);
-		$html = ob_get_contents();
-		@ob_end_clean();
 		$user = JFactory::getUser();
 		$loged = $user->id ? 1 : 0;
-		$str = $html.'_'.$loged.'_'.self::CVERSION;
+		$str = print_r($this->params, true).'_'.$loged.'_'.self::CVERSION;
 		$md5 = md5($str);
 		$this->file = $this->path.$md5.'.xml';
 		$this->expired = $this->loadXML($this->file);
-	}
-	function loadXML($file) {
-		if (!file_exists($file)) return true;
-		$this->xml = @simplexml_load_file($this->file);
-		if ($this->xml) {
-			if (!isset($this->xml->o_month) || !isset($this->xml->show) || !isset($this->xml->date) || !isset($this->xml->html)) {
-				return true;
-			}
-		}
-		else {
-			//malformed, load manually
-			$xml = file_get_contents($file);
-			$this->xml = new stdclass;
-			preg_match('#<o_month>(.*?)</o_month>#', $xml, $data); if (!$data) return true;
-			$this->xml->o_month = $data[1];
-			preg_match('#<show>(.*?)</show>#', $xml, $data); if (!$data) return true;
-			$this->xml->show = $data[1];
-			preg_match('#<date>(.*?)</date>#', $xml, $data); if (!$data) return true;
-			$this->xml->date = $data[1];
-			preg_match("#<html><!\\[CDATA\\[(.*?)\\]\\]></html>#", $xml, $data); if (!$data) return true;
-			$this->xml->html = $data[1];
-		}
-		return $this->xml->date < time() || !$this->xml->html;
 	}
 	function show() {
 		$this->show = $this->xml->show;
@@ -81,5 +56,28 @@ class modLcaCache {
 				mkdir($this->path);
 			file_put_contents($this->file, '<xml><o_month>'.$this->o_month.'</o_month><show>'.$this->show.'</show><date>'.(time()+$this->time).'</date><html><![CDATA['.$html.']]></html></xml>');
 		}
+	}
+	private function loadXML($file) {
+		if (!file_exists($file)) return true;
+		$this->xml = @simplexml_load_file($this->file);
+		if ($this->xml) {
+			if (!isset($this->xml->o_month) || !isset($this->xml->show) || !isset($this->xml->date) || !isset($this->xml->html)) {
+				return true;
+			}
+		}
+		else {
+			//malformed, load manually
+			$xml = file_get_contents($file);
+			$this->xml = new stdclass;
+			preg_match('#<o_month>(.*?)</o_month>#', $xml, $data); if (!$data) return true;
+			$this->xml->o_month = $data[1];
+			preg_match('#<show>(.*?)</show>#', $xml, $data); if (!$data) return true;
+			$this->xml->show = $data[1];
+			preg_match('#<date>(.*?)</date>#', $xml, $data); if (!$data) return true;
+			$this->xml->date = $data[1];
+			preg_match("#<html><!\\[CDATA\\[(.*?)\\]\\]></html>#", $xml, $data); if (!$data) return true;
+			$this->xml->html = $data[1];
+		}
+		return $this->xml->date < time() || !$this->xml->html;
 	}
 }
