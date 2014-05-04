6f33e26b28ff76aec04f811c196d927e3d03783f
JoniJnm <joni@jonijnm.es>
2014-05-04 18:20:59 +0200

    Atributor de caché no necesarios

--- cache.php
+++ cache.php
@@ -9,7 +9,6 @@ class modLcaCache {
 	const CVERSION = "f6";
 	
 	public $show;
-	public $o_month;
 	
 	private $params;
 	private $on;
@@ -54,14 +53,14 @@ class modLcaCache {
 				unlink($this->file);
 			if (!file_exists($this->path))
 				mkdir($this->path);
-			file_put_contents($this->file, '<xml><o_month>'.$this->o_month.'</o_month><show>'.$this->show.'</show><date>'.(time()+$this->time).'</date><html><![CDATA['.$html.']]></html></xml>');
+			file_put_contents($this->file, '<xml><show>'.$this->show.'</show><date>'.(time()+$this->time).'</date><html><![CDATA['.$html.']]></html></xml>');
 		}
 	}
 	private function loadXML($file) {
 		if (!file_exists($file)) return true;
 		$this->xml = @simplexml_load_file($this->file);
 		if ($this->xml) {
-			if (!isset($this->xml->o_month) || !isset($this->xml->show) || !isset($this->xml->date) || !isset($this->xml->html)) {
+			if (!isset($this->xml->show) || !isset($this->xml->date) || !isset($this->xml->html)) {
 				return true;
 			}
 		}
@@ -69,8 +68,6 @@ class modLcaCache {
 			//malformed, load manually
 			$xml = file_get_contents($file);
 			$this->xml = new stdclass;
-			preg_match('#<o_month>(.*?)</o_month>#', $xml, $data); if (!$data) return true;
-			$this->xml->o_month = $data[1];
 			preg_match('#<show>(.*?)</show>#', $xml, $data); if (!$data) return true;
 			$this->xml->show = $data[1];
 			preg_match('#<date>(.*?)</date>#', $xml, $data); if (!$data) return true;
