29373d3848af0bf2a6608a93082b2200b4afcd8d
JoniJnm <joni@jonijnm.es>
2014-05-04 12:40:41 +0200

    Eliminar código para Joomla 1.5

--- helper.php
+++ helper.php
@@ -10,12 +10,10 @@ defined('_JEXEC') or die('Restricted access');
 require_once (JPATH_SITE.'/components/com_content/helpers/route.php');
 
 class modLcaHelper {
-	var $params;
-	var $itemid;
-	var $j15;
+	private $params;
+	private $itemid;
 	
 	function __construct(&$params) {
-		$this->j15 = version_compare(JVERSION, "1.6.0", "<=");
 		$this->params = $params;
 		$this->itemid = $this->getItemid();
 	}
@@ -61,22 +59,12 @@ class modLcaHelper {
 		
 		if (!$this->params->get('show_pub_articles') && !$this->params->get('archived')) return $out;
 		
-		if ($this->j15) {
-			if ($this->params->get('show_pub_articles') && $this->params->get('archived'))
-				$state = '(a.state = -1 OR a.state = 1)';
-			else if ($this->params->get('show_pub_articles'))
-				$state = 'a.state = 1';
-			else
-				$state = 'a.state = -1';
-		}
-		else {
-			if ($this->params->get('show_pub_articles') && $this->params->get('archived'))
-				$state = '(a.state = 1 OR a.state = 2)';
-			else if ($this->params->get('show_pub_articles'))
-				$state = 'a.state = 1';
-			else
-				$state = 'a.state = 2';
-		}
+		if ($this->params->get('show_pub_articles') && $this->params->get('archived'))
+			$state = '(a.state = 1 OR a.state = 2)';
+		else if ($this->params->get('show_pub_articles'))
+			$state = 'a.state = 1';
+		else
+			$state = 'a.state = 2';
 		
 		$order = $this->params->get("order", 2);
 		if ($order == 0) //created
@@ -86,34 +74,25 @@ class modLcaHelper {
 		else //publised
 			$order = 'a.publish_up AS co,';
 		
-		$section = $this->j15 ? 'a.sectionid,' : '';
-		if ($this->j15)
-			$secs = trim($this->params->get("secs", "")) ? " AND a.sectionid IN (".$this->params->get("secs", "").")" : "";
-		else
-			$secs = '';
 		$cats = trim($this->params->get("cats", "")) ? " AND a.catid IN (".$this->params->get("cats", "").")" : "";
 		$maxyears = $this->params->get("maxyears", 0);
 		
 		if ($user->id)
-			$access = ' AND (a.access='.($this->j15?0:1).' OR a.access='.($this->j15?1:2).')';
+			$access = ' AND (a.access=1 OR a.access=2)';
 		else
-			$access = ' AND (a.access='.($this->j15?0:1).')';
+			$access = ' AND (a.access=1)';
 		
 		$o_article = $this->params->get("o_article", "desc");
 		$query = 'SELECT '.
 			$order.' '.
-			$section.' '.
 			'a.id, a.title, a.alias, a.catid, c.alias as calias'.
 			' FROM #__content AS a'.
 			' LEFT JOIN #__categories AS c ON c.id=a.catid'.
-			($this->j15 ? ' LEFT JOIN #__sections AS s ON s.id=a.sectionid' : '').
-			' WHERE ('.$state.' '.($this->j15?'AND s.id > 0':'').')' .
+			' WHERE ('.$state.')' .
 			$access.
 			' AND ( a.publish_up = '.$db->Quote($nullDate).' OR a.publish_up <= '.$db->Quote($now).' )'.
 			' AND ( a.publish_down = '.$db->Quote($nullDate).' OR a.publish_down >= '.$db->Quote($now).' )'.
-			($this->j15?' AND s.published = 1':'').
 			' AND c.published = 1'.
-			$secs.
 			$cats.
 			' ORDER BY co '.$o_article;
 		if ($this->params->get("maxarticles", 150) > 0)
@@ -154,10 +133,7 @@ class modLcaHelper {
 			
 			if (isset($out->articulos[$d])) {
 				$out->lastyear = $d;
-				if ($this->j15)
-					$url = ContentHelperRoute::getArticleRoute($row->id.":".$row->alias, $row->catid.":".$row->calias, $row->sectionid);
-				else
-					$url = ContentHelperRoute::getArticleRoute($row->id.":".$row->alias, $row->catid.":".$row->calias);
+				$url = ContentHelperRoute::getArticleRoute($row->id.":".$row->alias, $row->catid.":".$row->calias);
 				if ($this->itemid && strpos($url, "&Itemid=") === false) $url .= '&Itemid='.$this->itemid;
 				$link = JRoute::_($url);
 				$month = $this->monthToString($m);
@@ -177,26 +153,16 @@ class modLcaHelper {
 	
 	private function getItemid() {
 		$db = JFactory::getDBO();
-		if ($this->j15) {
-			$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=frontpage" AND home=1');
-			$id = $db->loadResult();
-			if ($id) return $id;
-			$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=frontpage" AND access=0 AND published=1 ORDER BY id DESC LIMIT 1');
-			$id = $db->loadResult();
-			if ($id) return $id;
-			$db->setQuery('SELECT id FROM #__menu WHERE link LIKE "index.php?option=com_content%" AND access=0 AND published=1 ORDER BY id DESC LIMIT 1');
-			$id = $db->loadResult();
-		}
-		else {
-			$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=featured" AND home=1');
-			$id = $db->loadResult();
-			if ($id) return $id;
-			$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=featured" AND access<=1 AND published=1 ORDER BY id DESC LIMIT 1');
-			$id = $db->loadResult();
-			if ($id) return $id;
-			$db->setQuery('SELECT id FROM #__menu WHERE link LIKE "index.php?option=com_content%" AND access<=1 AND published=1 ORDER BY id DESC LIMIT 1');
-			$id = $db->loadResult();
-		}
+		
+		$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=featured" AND home=1');
+		$id = $db->loadResult();
+		if ($id) return $id;
+		$db->setQuery('SELECT id FROM #__menu WHERE link="index.php?option=com_content&view=featured" AND access<=1 AND published=1 ORDER BY id DESC LIMIT 1');
+		$id = $db->loadResult();
+		if ($id) return $id;
+		$db->setQuery('SELECT id FROM #__menu WHERE link LIKE "index.php?option=com_content%" AND access<=1 AND published=1 ORDER BY id DESC LIMIT 1');
+		$id = $db->loadResult();
+		
 		if ($id) return $id;
 		return 0;
 	}
